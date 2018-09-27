050fcbde7cae2e676ea18f5c75b7ba0df93c1e38
Oksana Sanag <oksanasanag@gmail.com>
2018-09-27 18:05:00 +0300

    change StatusBar

--- source/components/StatusBar/index.js
+++ source/components/StatusBar/index.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 
 //Instruments
 import Styles from './styles.m.css';
-import avatar from 'theme/assets/lisa';
 
 export default class StatusBar extends Component {
     render () {
