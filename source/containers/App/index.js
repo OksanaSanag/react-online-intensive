0576f5c7b85ddc24ee71be93645e86be701992e6
Oksana Sanag <oksanasanag@gmail.com>
2018-09-28 16:42:42 +0300

    react-online-3-lesson-4

--- source/containers/App/index.js
+++ source/containers/App/index.js
@@ -1,9 +1,9 @@
 // Core
 import React, { Component } from 'react';
-import { hot } from 'react-hot-loader';
 
 //Components
 import Feed from 'components/Feed';
+import { Provider } from 'components/HOC/withProfile';
 
 //Instruments
 import avatar from "theme/assets/lisa";
@@ -14,9 +14,12 @@ const options = {
     currentUserLastName:  'Simpson',
 };
 
-@hot(module)
 export default class App extends Component {
     render () {
-        return <Feed { ...options } />;
+        return (
+            <Provider value = { options }>
+                <Feed />
+            </Provider>
+        );
     }
 }
