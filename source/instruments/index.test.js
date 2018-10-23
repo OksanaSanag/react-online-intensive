fb9f44f959b339e82066b5e7f09dbd956bf5569d
Oksana Sanag <oksanasanag@gmail.com>
2018-10-23 22:59:13 +0300

    react-online-3-lesson-11

---
+++ source/instruments/index.test.js
@@ -0,0 +1,50 @@
+// Core
+import { sum, delay, getUniqueID, getFullApiUrl } from './';
+
+describe('instruments:', () => {
+    test('sum function should be a function', () => {
+        expect(sum).toBeInstanceOf(Function);
+    });
+    test('sum function should throw, when called with non-number type as second argument', () => {
+        expect(() => sum(2, 'hello')).toThrow();
+    });
+
+    test('sum function should throw, when called with non-number type as first argument', () => {
+        expect(() => sum('hello', 2)).toThrow();
+    });
+
+    test('sum function should return an addition of two arguments passed', () => {
+        expect(sum(2, 3)).toBe(5);
+        expect(sum(1, 8)).toMatchSnapshot();
+    });
+
+    test('delay function should return a resolved promise', async () => {
+        await expect(delay()).resolves.toBeUndefined();
+    });
+
+    test('getUniqueID function should be a function', () => {
+        expect(getUniqueID).toBeInstanceOf(Function);
+    });
+
+    test('getUniqueID function should throw, when called with non-number type as an argument', () => {
+        expect(() => getUniqueID('hello')).toThrow();
+    });
+
+    test('getUniqueID function should produce a string of a desired given length', () => {
+        expect(typeof getUniqueID()).toBe('string');
+        expect(getUniqueID(5)).toHaveLength(5);
+        expect(getUniqueID(13)).toHaveLength(13);
+    });
+
+    test('getFullApiUrl function should be a function', () => {
+        expect(getFullApiUrl).toBeInstanceOf(Function);
+    });
+
+    test('getFullApiUrl function should throw, when called with non-string type as first argument', () => {
+        expect(() => getFullApiUrl(2, 'hi')).toThrow();
+    });
+
+    test('getFullApiUrl function should throw, when called with non-string type as second argument', () => {
+        expect(() => getFullApiUrl('hi', 2)).toThrow();
+    });
+});
